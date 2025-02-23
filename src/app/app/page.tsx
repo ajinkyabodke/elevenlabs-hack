a478b0404c08820ff0ad6905fe70c6bbb4affff8
Punarv Dinakar <tranquilblue91@gmail.com>
2025-02-23 07:16:50 +0530

    minor ui

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -118,6 +118,7 @@ export default function Home() {
       },
       end_session: async () => {
         console.log("Stopping recording...");
+        setIsRecording(false);
         setIsProcessing(true);
         await conversation.endSession();
         setTranscript({ messages: [] });
